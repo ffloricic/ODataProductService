50259b196339a7fb0c3f63dac76339e262b2bf52
Flavio Floričić <flavio.floricic@uniline.hr>
2018-01-19 13:34:07 +0100

    OData Product Service - prvi commit 	vroductService.sln 	# Please enter the commit message for your changes. Lines starting 	# with '#' will be ignored, and an empty message aborts the commit. 	# 	# On branch master 	# 	# Initial commit 	# 	# Changes to be committed: 	#	new file:   .gitignore 	#	new file:   .vs/ProductService/v15/.suo 	#	new file:   .vs/ProductService/v15/Server/sqlite3/storage.ide 	#	new file:   .vs/ProductService/v15/Server/sqlite3/storage.ide-shm 	#	new file:   .vs/ProductService/v15/Server/sqlite3/storage.ide-wal 	#	new file:   .vs/config/applicationhost.config 	#	new file:   ProductService.sln 	#	new file:   ProductService/App_Data/ProductServiceContext-20180119114800.mdf

---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using ProductService.Models;
+
+namespace ProductService.Controllers
+{
+    /*
+    The WebApiConfig class may require additional changes to add a route for this controller. Merge these statements into the Register method of the WebApiConfig class as applicable. Note that OData URLs are case sensitive.
+
+    using System.Web.Http.OData.Builder;
+    using System.Web.Http.OData.Extensions;
+    using ProductService.Models;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Product>("Products");
+    config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class ProductsController : ODataController
+    {
+        private ProductServiceContext db = new ProductServiceContext();
+
+        // GET: odata/Products
+        [EnableQuery]
+        public IQueryable<Product> GetProducts()
+        {
+            return db.Products;
+        }
+
+        // GET: odata/Products(5)
+        [EnableQuery]
+        public SingleResult<Product> GetProduct([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Products.Where(product => product.ID == key));
+        }
+
+        // PUT: odata/Products(5)
+        public IHttpActionResult Put([FromODataUri] int key, Delta<Product> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Product product = db.Products.Find(key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(product);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(product);
+        }
+
+        // POST: odata/Products
+        public IHttpActionResult Post(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return Created(product);
+        }
+
+        // PATCH: odata/Products(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Product> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Product product = db.Products.Find(key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(product);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(product);
+        }
+
+        // DELETE: odata/Products(5)
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            Product product = db.Products.Find(key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Products.Remove(product);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        //GET /products(1)/SUpplier
+        public Supplier GetSupplier([FromODataUri] int key)
+        {
+            Product product = db.Products.FirstOrDefault(p => p.ID == key);
+            if(product == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return product.Supplier;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductExists(int key)
+        {
+            return db.Products.Count(e => e.ID == key) > 0;
+        }
+    }
+}
---
+++ ProductService/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProductService.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+        [ForeignKey("Supplier")]
+        public string SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+    }
+}
